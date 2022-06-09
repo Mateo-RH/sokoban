7d7df31c0d0b4f597e3888b0d11d599e23d49a39
MateoRH <mateo.1459@hotmail.com>
2022-06-09 16:31:14 -0500

    add components

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
+mod components;
+use components::*;
+
 use ggez::{conf, event, Context, GameResult};
+use specs::{ReadStorage, System, World, WorldExt};
 use std::path;
+
+pub struct RenderingSystem<'a> {
+    context: &'a mut Context,
+}
+
+impl<'a> System<'a> for RenderingSystem<'a> {
+    type SystemData = (ReadStorage<'a, Position>, ReadStorage<'a, Renderable>);
+
+    fn run(&mut self, (positions, renderables): Self::SystemData) {}
+}
 
 // This struct will hold all our game state
 // For now there is nothing to be held, but we'll add
 // things shortly.
-struct Game {}
+struct Game {
+    world: World,
+}
 
 // This is the main event loop. ggez tells us to implement
 // two things:
@@ -16,13 +32,18 @@ impl event::EventHandler<ggez::GameError> for Game {
         Ok(())
     }
 
-    fn draw(&mut self, _context: &mut Context) -> GameResult {
-        // TODO: update draw here
+    fn draw(&mut self, context: &mut Context) -> GameResult {
+        {
+            let mut rs = RenderingSystem { context };
+            // rs.run_now(&self.world);
+        }
         Ok(())
     }
 }
 
 pub fn main() -> GameResult {
+    let mut world = World::new();
+
     // Create a game context and event loop
     let context_builder = ggez::ContextBuilder::new("rust_sokoban", "sokoban")
         .window_setup(conf::WindowSetup::default().title("Rust Sokoban!"))
@@ -31,7 +52,7 @@ pub fn main() -> GameResult {
 
     let (context, event_loop) = context_builder.build()?;
     // Create the game state
-    let game = Game {};
+    let game = Game { world };
     // Run the main event loop
     event::run(context, event_loop, game)
 }
